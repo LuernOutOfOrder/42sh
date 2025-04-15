7f2ee466482a6428d5329e1fde0f460a66c1cdc6
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-04-15 22:39:47 +0200

    feat: update .gitignore and fix exit

--- main.c
+++ main.c
@@ -1,20 +1,17 @@
+#include "src/parser.c"
 #include <stdio.h>
 #include <stdlib.h>
-#include "src/parser.c"
 #include <string.h>
 
-int main()
-{
-    for (;;)
-    {
-        char my_string[10];
-        printf("42sh>");
-        s_gets(my_string, 10);
-        printf("%s\n", my_string);
-        if (strcmp(my_string, "exit\0") != 0)
-        {
-            exit(0);
-        }
+int main() {
+  for (;;) {
+    char my_string[10];
+    printf("42sh>");
+    s_gets(my_string, 10);
+    printf("%s\n", my_string);
+    if (strcmp("exit", my_string) == 0) {
+      exit(0);
     }
-    return 0;
+  }
+  return 0;
 }
